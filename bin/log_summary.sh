d3f79639fe3556d05d5553f7832aaee7b590a42b
ayoraind <afolayanayorinde@gmail.com>
2023-10-12 08:59:20 +0200

    added more %s to account for more columns added

--- bin/log_summary.sh
+++ bin/log_summary.sh
@@ -28,18 +28,18 @@ N50_FinalIteration=$(cat ${log_file} | awk 'NR == 49 {print $2}')
 elif grep -q "Iteration #1" ${log_file}
 then
 
-contig_number_input=$(cat ${log_file} | awk 'NR == 32 {print $2}')
-contig_length_input=$(cat ${log_file} | awk 'NR == 33 {print $2}')
-N50_input=$(cat ${log_file} | awk 'NR == 34 {print $2}')
-contig_number_iteration1=$(cat ${log_file} | awk 'NR == 37 {print $2}')
-contig_length_iteration1=$(cat ${log_file} | awk 'NR == 38 {print $2}')
-N50_iteration1=$(cat ${log_file} | awk 'NR == 39 {print $2}')
+contig_number_input=$(cat ${log_file} | awk 'NR == 25 {print $2}')
+contig_length_input=$(cat ${log_file} | awk 'NR == 26 {print $2}')
+N50_input=$(cat ${log_file} | awk 'NR == 27 {print $2}')
+contig_number_iteration1=$(cat ${log_file} | awk 'NR == 30 {print $2}')
+contig_length_iteration1=$(cat ${log_file} | awk 'NR == 31 {print $2}')
+N50_iteration1=$(cat ${log_file} | awk 'NR == 32 {print $2}')
 contig_number_iteration2="NA"
 contig_length_iteration2="NA"
 N50_iteration2="NA"
-contig_number_output=$(cat ${log_file} | awk 'NR == 42 {print $2}')
-contig_length_output=$(cat ${log_file} | awk 'NR == 43 {print $2}')
-N50_output=$(cat ${log_file} | awk 'NR == 44 {print $2}')
+contig_number_FinalIteration=$(cat ${log_file} | awk 'NR == 35 {print $2}')
+contig_length_FinalIteration=$(cat ${log_file} | awk 'NR == 36 {print $2}')
+N50_FinalIteration=$(cat ${log_file} | awk 'NR == 37 {print $2}')
 
 # If none of the patterns are present
 else
@@ -59,4 +59,4 @@ N50_FinalIteration=$(cat ${log_file} | awk 'NR == 25 {print $2}')
 
 fi
 
-printf "%s\t%s\t%s\t%s\t%s\t%s\n" $FILE $contig_number_input $contig_length_input $N50_input $contig_number_iteration1 $contig_length_iteration1 $N50_iteration1 $contig_number_iteration2 $contig_length_iteration2 $N50_iteration2 $contig_number_FinalIteration $contig_length_FinalIteration $N50_FinalIteration >> ${FILE}.logsummary
+printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" $FILE $contig_number_input $contig_length_input $N50_input $contig_number_iteration1 $contig_length_iteration1 $N50_iteration1 $contig_number_iteration2 $contig_length_iteration2 $N50_iteration2 $contig_number_FinalIteration $contig_length_FinalIteration $N50_FinalIteration >> ${FILE}.logsummary
